8ecb5c96aacc9da04c8467292e14d7b64e1412ae
root <badreddineelmoutarif@gmail.com>
2023-11-21 12:18:43 +0100

    All tasks finished! 🏁

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
