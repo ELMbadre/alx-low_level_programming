a10b5b4ba3e9fba6eecaa23b226a6df161103be8
root <badreddineelmoutarif@gmail.com>
2023-11-21 11:55:52 +0100

    All tasks finished! 🏁

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ *
+ * @n: The number of _ characters to be printed.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
