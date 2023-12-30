42da115102dee5998489211a6192477c81ed376c
root <badreddineelmoutarif@gmail.com>
2023-12-30 21:13:09 +0100

    All tasks finished! 🏁

--- badr.sh
+++ badr.sh
@@ -5,14 +5,14 @@ USERNAME="your_username"
 REPO_NAME="your_repository"
 
 # Create 33 files
-for i in {1..2000}
+for i in {1..53}
 do
     touch "file${i}.c"
     echo "// This is file ${i}" >> "file${i}.c"
 done
 
 # Add, commit, and push each file
-for i in {1..2000}
+for i in {1..53}
 do
     git add "file${i}.c"
     git commit -m "Add file${i}.c"
