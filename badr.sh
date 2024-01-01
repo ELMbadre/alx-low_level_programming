0a8bf87187086a850024f78d0bb686bae0ce3835
root <badreddineelmoutarif@gmail.com>
2024-01-01 04:37:34 +0100

    All tasks finished! 🏁

---
+++ badr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set your GitHub username and repository name
+USERNAME="your_username"
+REPO_NAME="your_repository"
+
+# Create 33 files
+for i in {1..33}
+do
+    touch "file${i}.c"
+    echo "// This is file ${i}" >> "file${i}.c"
+done
+
+# Add, commit, and push each file
+for i in {1..33}
+do
+    git add "file${i}.c"
+    git commit -m "Add file${i}.c"
+    git push origin main
+done
+
