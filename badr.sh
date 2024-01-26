346ed44100ec6e84aa449e1af604f1bca729d656
ELMbadre <badreddineelmoutarif@gmail.com>
2024-01-26 01:00:38 +0100

    All tasks finished! 🏁

--- badr.sh
+++ badr.sh
@@ -5,14 +5,14 @@ USERNAME="your_username"
 REPO_NAME="your_repository"
 
 
-for i in {1..70}
+for i in {1..2}
 do
     touch "file${i}.py"
     echo "// This is file ${i}" >> "file${i}.py"
 done
 
 # Add, commit, and push each file
-for i in {1..70}
+for i in {1..2}
 do
     git add "file${i}.py"
     git commit -m "All tasks finished! 🏁"
